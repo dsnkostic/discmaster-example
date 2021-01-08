a5852a04276ccd8f2c0c82299e31ee4e6dc31659
Dusan Kostic <dsnkostic@gmail.com>
2021-01-08 17:15:45 +0100

    First commit

---
+++ src/main/java/org/dsnkostic/discmaster/DiscmasterExampleApplication.java
@@ -0,0 +1,12 @@
+package org.dsnkostic.discmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscmasterExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiscmasterExampleApplication.class, args);
+  }
+}
